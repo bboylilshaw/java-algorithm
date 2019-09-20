2f378f164dd9d2296983221b25b5db3bccd7c0c8
Jason Xiao <xiaoyao8823@gmail.com>
2019-09-20 18:53:32 +0800

    add 'find Kth largest element in array'

---
+++ src/io/xiaoyao/algorithm/KthLargestElementInArray.java
@@ -0,0 +1,76 @@
+package io.xiaoyao.algorithm;
+
+import io.xiaoyao.algorithm.common.SwapUtil;
+
+import javax.swing.*;
+import java.util.PriorityQueue;
+
+/**
+ * @author Jason Xiao
+ * @date 2019/9/20
+ */
+public class KthLargestElementInArray {
+
+
+    /**
+     * 方法1
+     * 快排的变种，在快排寻找pivot的过程中找到第k大的数字
+     */
+    public static int find(int[] arr, int k) {
+        if (k > arr.length) throw new RuntimeException();
+        return find(arr, 0, arr.length - 1, k);
+    }
+
+    public static int find(int[] arr, int left, int right, int k) {
+        int index = partition(arr, 0, arr.length - 1);
+        while (index != arr.length - k) {
+            if (index > arr.length - k) {
+                index = partition(arr, left, index - 1);
+            } else {
+                index = partition(arr, index + 1, right);
+            }
+        }
+        return arr[index];
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivotPos = left;
+        int pivot = arr[left];
+        while (left < right) {
+            while (left < right && arr[right] >= pivot) {
+                right--;
+            }
+            while (left < right && arr[left] <= pivot) {
+                left++;
+            }
+
+            SwapUtil.swap(arr, left, right);
+        }
+        SwapUtil.swap(arr, pivotPos, left);
+        return pivotPos;
+    }
+
+
+    /**
+     * 方法2
+     * 用最小顶堆实现，一个个数字加到堆里，堆里保持k个元素，遍历完，堆顶就是第k大的数字
+     *
+     * ps，取第k大的用小顶堆，取第k小的用大顶堆
+     */
+    public static int findKth(int[] arr, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i : arr) {
+            queue.add(i);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        return queue.poll();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{4, 5, 1, 2, 3, 4, 4, 4, 7};
+        System.out.println(find(nums, 2));//第二大应该输出5
+        System.out.println(findKth(nums, 2));//第二大应该输出5
+    }
+}
---
+++ src/io/xiaoyao/algorithm/common/SwapUtil.java
@@ -0,0 +1,13 @@
+package io.xiaoyao.algorithm.common;
+
+/**
+ * @author Jason Xiao
+ * @date 2019/9/20
+ */
+public class SwapUtil {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
